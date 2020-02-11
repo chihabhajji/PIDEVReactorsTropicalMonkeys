a3f427c30493af4e8fb5905ce2fcdd97ea8fec3d
chihabhajji <blackpang23@gmail.com>
2020-02-11 21:07:06 +0100

    Backend template

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,4 +19,15 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/back", name="backend")
+     */
+    public function backAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('baseback.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
 }
