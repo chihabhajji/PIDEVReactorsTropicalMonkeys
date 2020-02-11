566ac7ef00ce9afa87e43516ecaf283270c29559
chihabhajji <blackpang23@gmail.com>
2020-02-11 21:11:25 +0100

    Fixed Mission CRUD

--- src/AppBundle/Controller/MissionController.php
+++ src/AppBundle/Controller/MissionController.php
@@ -2,21 +2,135 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\User;
+use AppBundle\Entity\Mission;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Mission controller.
+ *
+ * @Route("mission")
+ */
 class MissionController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * Lists all mission entities.
+     *
+     * @Route("/", name="mission_index")
+     * @Method("GET")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $em = $this->getDoctrine()->getManager();
+
+        $missions = $em->getRepository('AppBundle:Mission')->findAll();
+
+        return $this->render('mission/index.html.twig', array(
+            'missions' => $missions,
+        ));
+    }
+
+    /**
+     * Creates a new mission entity.
+     *
+     * @Route("/new", name="mission_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $mission = new Mission();
+        $form = $this->createForm('AppBundle\Form\MissionType', $mission);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($mission);
+            $em->flush();
+
+            return $this->redirectToRoute('mission_show', array('id' => $mission->getId()));
+        }
+
+        return $this->render('mission/new.html.twig', array(
+            'mission' => $mission,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a mission entity.
+     *
+     * @Route("/{id}", name="mission_show")
+     * @Method("GET")
+     */
+    public function showAction(Mission $mission)
+    {
+        $deleteForm = $this->createDeleteForm($mission);
+
+        return $this->render('mission/show.html.twig', array(
+            'mission' => $mission,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing mission entity.
+     *
+     * @Route("/{id}/edit", name="mission_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Mission $mission)
+    {
+        $deleteForm = $this->createDeleteForm($mission);
+        $editForm = $this->createForm('AppBundle\Form\MissionType', $mission);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('mission_edit', array('id' => $mission->getId()));
+        }
+
+        return $this->render('mission/edit.html.twig', array(
+            'mission' => $mission,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a mission entity.
+     *
+     * @Route("/{id}", name="mission_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Mission $mission)
+    {
+        $form = $this->createDeleteForm($mission);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($mission);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('mission_index');
+    }
+
+    /**
+     * Creates a form to delete a mission entity.
+     *
+     * @param Mission $mission The mission entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Mission $mission)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('mission_delete', array('id' => $mission->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
     }
 }
